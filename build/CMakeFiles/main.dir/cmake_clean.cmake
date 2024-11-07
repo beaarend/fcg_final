f82768cb6854b70543c8706a43f6162c2dea781f
jo2099 <fleckjoaocarlos@gmail.com>
2024-11-07 11:17:54 -0300

    Initial Layou based on Lab 2

---
+++ build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "/home/joao/faculdade/FCG/fcg_final/bin/Linux/main"
+  "/home/joao/faculdade/FCG/fcg_final/bin/Linux/main.pdb"
+  "CMakeFiles/main.dir/src/glad.c.o"
+  "CMakeFiles/main.dir/src/glad.c.o.d"
+  "CMakeFiles/main.dir/src/main.cpp.o"
+  "CMakeFiles/main.dir/src/main.cpp.o.d"
+  "CMakeFiles/main.dir/src/textrendering.cpp.o"
+  "CMakeFiles/main.dir/src/textrendering.cpp.o.d"
+  "CMakeFiles/main.dir/src/tiny_obj_loader.cc.o"
+  "CMakeFiles/main.dir/src/tiny_obj_loader.cc.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
